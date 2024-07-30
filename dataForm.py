67a1a2b08a07c5a517262a990a2a14d19c43b80a
Dimifabbridf96 <dimi.fabbri@gmail.com>
2024-07-30 13:49:16 +0100

    added Flask-WTForm to create my form and created dataForm.py to build the form

---
+++ dataForm.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, SelectField, SubmitField
+from wtforms.validators import DataRequired
+
+class Form(FlaskForm):
+    amount = IntegerField("Amount",validators=[DataRequired])
+    type = SelectField("Type", validators=[DataRequired()],
+                            choices=[('expense', 'expense'), ('income', 'income')])
+    category = SelectField("Category", validators=[DataRequired()],
+                           choices=[('rental', 'rental'), ('salary', 'salary'), ('shopping', 'shopping'),
+                                    ('amusement', 'amusement'),('crypto', 'crypto'),('other', 'other')])
+    submit =SubmitField("Submit")
+    
