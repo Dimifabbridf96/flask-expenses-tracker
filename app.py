2d20a6f82d2deb69a2e6daff09181b88bf1cb8b7
Dimifabbridf96 <dimi.fabbri@gmail.com>
2024-07-30 11:44:52 +0100

    added db with sqlite, initialized and configuration

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///expenses.db'
+
+db.init_app(app)
+
 
 @app.route('/')
 def index():
